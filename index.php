68fb9edd34e0a7ac731a3852dea0f90605577b31
Jack Waterworth <jwaterwo@redhat.com>
2015-04-21 16:56:45 -0400

    adding basic index.php file to get started

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    // this is a PHP file, so lets make sure we have our open and close to build off of.
+    // please use 4 spaces for code spacing.
+?>
+<html>
+
+    <head>
+        <title>art-fight</title>
+    </head>
+
+    <body>
+
+        Body text goes here!
+
+    </body>
+</html>>
