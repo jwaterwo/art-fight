8b721d7745c0b64513645d84c9b582e3dfef0d63
Jack Waterworth <jwaterworth@gmail.com>
2015-04-26 22:19:11 -0400

    creating a base template

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-8====D
+<?php
+    // this is a PHP file, so lets make sure we have our open and close to build off of.
+    // please use 4 spaces for code spacing.
+?>
+<html>
+    <head>
+        <title>art-fight</title>
+    </head>
+    <body>
+      art-fight goes here.
+    </body>
+</html>
