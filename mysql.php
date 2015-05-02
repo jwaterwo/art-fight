5da1347f6c113dcf35f66bfb6a606445efa0d146
Jack Waterworth <jwaterworth@gmail.com>
2015-05-02 12:27:54 -0400

    built the basic mysql variables page

--- mysql.php
+++ mysql.php
@@ -1,4 +1,16 @@
 <?php
     // this file is used to specify the details for the mysql server
     // it can be modified manually, but should be initially setup by the setup.php script
+    
+    // if this file is not setup, we should redirect to setup.php
+
+    // here are some example entires. we'll need some way to determine if they are "not set up" so we can redirect
+    $username="username";
+    $password="password";
+    $database="database"; // this will need to be unique per instance of art-fight
+
+    //connect to mysql database
+    mysql_connect("localhost",$username,$password);
+    @mysql_select_db($database) or die( "Unable to select database");
 ?>
+
