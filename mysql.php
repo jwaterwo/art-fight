a948981cd7c75af9ecaeb50b9e34d4b692662301
Jack Waterworth <jwaterworth@gmail.com>
2015-05-02 22:11:01 -0400

    added basic logic template to setup page

--- mysql.php
+++ mysql.php
@@ -3,22 +3,20 @@
     // do not modify this file. mysql details can be found in mysql-cfg.php
     
     // check to see if mysql config file exists. load or redirect
-    if (file_exists(mysql-cfg.php)) {
+    if (file_exists("mysql-cfg.php")) {
         $mysql = include 'mysql-cfg.php';
     } else {
         header('Location: setup.php');
     }
 
     // here are some example entires. we'll need some way to determine if they are "not set up" so we can redirect
+    $hostname=$mysql['hostname'];
     $username=$mysql['username'];
     $password=$mysql['password'];
     $database=$mysql['database']; // this will need to be unique per instance of art-fight
 
-    // testing line. needs to be removed
-    echo $username . "<br>" . $password . "<br>" . $database;
-
     //connect to mysql database
-    mysql_connect("localhost",$username,$password);
+    mysql_connect($hostname,$username,$password);
     @mysql_select_db($database) or die( "Unable to select database");
 ?>
 
--- setup.php
+++ setup.php
@@ -1,6 +1,32 @@
 <?php
-    // this will be used to setup the mysql database, and the first user
+    // this will be used to setup/modify the mysql database, and the main admin user
     
     //temp line. this should be removed
     echo "if you are seeing this page, art-fight has not been setup correctly";
+
+    // load up any existing values
+    if (file_exists(mysql-cfg.php)) {
+        $mysql = include 'mysql-cfg.php';
+    }
+
+    // a form to SET the volumes shoudl be created here, and display the pulled values (if they exist)
+    /*
+    $mysql['hostname']
+    $mysql['username']
+    $mysql['password']
+    $mysql['database']
+    */
+
+    /* current hiding this, but eventually we'll need to setup a forum with the values from the below 
+    // set the mysql variables from the form
+    $mysql['hostname']= 'localhost';
+    $mysql['username']= 'jack';
+    $mysql['password']= 'ganymede';
+    $mysql['database']= 'art-fight';
+    if (file_put_contents('mysql-cfg.php', '<?php return ' . var_export($mysql, true) . ';')){
+        // this runs on success to write file
+    }else{
+        // this runs upon failure to write file
+    }
+    */
 ?>
