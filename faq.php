85cebcc33c71cc741c7bd6078ccdc205b177f1c2
Kelsey Waterworth <chococabbit@gmail.com>
2015-05-13 21:23:58 -0400

    added faq page, needs body text

---
+++ faq.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+		<title>FAQ</title>
+    		<link rel="stylesheet" href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css">
+    		<link rel="stylesheet" href="main.css">
+	</head>
+	<body>
+<div class="nav">
+      <div class="container">
+        <ul class="pull-left">
+          <li><a href="/index_kelsey.php">Home</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+        <ul class="pull-right">
+          <li><a href="#">Register</a></li>
+          <li><a href="#">Log In</a></li>
+          <li><a href="/faq.php">FAQ</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Art Fight 2015</h1>
+	        <p>Two Weeks. Two Teams. One Winner.</p>
+	        <a href="#">Learn More</a>
+      </div>
+   </div> 
+
+    <div class="guides">
+        <div class="container">
+   	    <h2> FAQ GOES HERE</h2>
+    </div>
+         <div class="learn-more">
+	  <div class="container">
+		<div class="row">
+	      <div class="col-md-4">
+			<h3>Teams</h3>
+				<p>Help us decide on this years teams!</p>
+				<p><a href="#">vote on teams for 2015</a></p>
+	      </div>
+		 <div class="col-md-4">
+			<h3>Oppurtunities</h3>
+				<p>Want to be a mod? Provide site art? </p>
+				<p><a href="/application.php">Apply to help out Chronic and WN</a></p>
+		  </div>
+		  <div="col-md-4">
+			<h3>Rules and Regulations</h3>
+				<p>How to play the game, and a few simple rules</p>
+				<p><a href="#">Read the Guide</a></p>
+		  </div>
+	    </div>
+	  </div>
+	</div>
+	</body>
+</html>
